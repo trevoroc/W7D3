55b4f69ee80de5dd79b0185900c900a2ca42587f
Trevor OConnor <trevoroc22@gmail.com>
2017-05-10 14:43:46 -0700

    Add middleware, add functional action

--- pokedex/frontend/pokedex.jsx
+++ pokedex/frontend/pokedex.jsx
@@ -5,11 +5,11 @@ import configureStore from './store/store';
 
 // TODO: Delete later
 import { fetchAllPokemon } from './util/api_util';
-import { receiveAllPokemon } from './actions/pokemon_actions';
+import { requestAllPokemon, receiveAllPokemon } from './actions/pokemon_actions';
 
 window.fetchAllPokemon = fetchAllPokemon;
 window.receiveAllPokemon = receiveAllPokemon;
-
+window.requestAllPokemon = requestAllPokemon;
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
--- pokedex/frontend/store/store.js
+++ pokedex/frontend/store/store.js
@@ -1,6 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
+import thunkMiddleware from '../middleware/thunk';
 
-const configureStore = () => createStore(rootReducer);
+const configureStore = (preLoadedState = {}) => (
+  createStore(rootReducer,
+    preLoadedState,
+    applyMiddleware(thunkMiddleware))
+);
 
 export default configureStore;
